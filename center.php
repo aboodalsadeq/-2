68ddc92efb6d9c86382b56bbc11d66b4501a96c6
aboodalsadeq <132617101+aboodalsadeq@users.noreply.github.com>
2023-09-03 14:12:07 +0300

    Add files via upload

---
+++ center.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ICE COMMUNTIE</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+<header>
+
+    <a href="http://www.ttu.edu.jo/"><img src="ttuphoto.jpg" class="logo"></a>
+    <img src="user_1177568.png" class="memberphoto">
+    <nav class="navigation">
+        <a href="search.php">search</a>
+        <a href="index.html">الصفحة الرئيسة </a>
+        <a href="contact.html">للتواصل معنا</a>
+    </nav>
+</header>
+<section class="center">
+    <div>
+        <p>
+            ؟ICE من لجنة ال
+        </p>
+        <pre>
+                   هي لجنة تطوعية تندرج تحت مظلة فريق 
+                 جيل النهضة هدف هذه اللجنة هو مساعدة  
+            طلاب هندسة الأنظمة الذكية والحاسوب والإتصلات 
+        </pre>
+        <img src="ICE.png" class="ice">
+    </div>
+</section>
+<section class="cards" id="services">
+    <h1>خدماتنا</h1>
+    <div class="content">
+        <div class="boxs">
+            <a href="https://www.youtube.com/@NaeemAlOudat">
+                <div class="icon">
+                    <i class="fa-solid fa-circle-play"></i>
+                </div>
+                <div class="text">
+                    <h2>Videos</h2>
+                </div>
+            </a>
+        </div>
+
+        <div class="boxs">
+            <a href="sugg.php">
+                <div class="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </div>
+                <div class="text">
+                    <h2>أقتراح مواد الفصل الأول</h2>
+                </div>
+            </a>
+        </div>
+
+        <div class="boxs">
+            <a href="summary.html">
+                <div class="icon">
+                    <i class="fas fa-graduation-cap"></i>
+                </div>
+                <div class="text">
+                    <h2>الملخصات</h2>
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+</section>
+
+<body>
+</body>
+
+</html>
